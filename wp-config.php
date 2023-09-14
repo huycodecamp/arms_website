7e11a5e0aa2598b8482771a22e8a368ce6f76e43
Huybc <huy.bc@aptechlearning.edu.vn>
2023-09-14 12:15:55 +0700

    commit code API WP

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'arms' );
+define( 'DB_NAME', 'wordpress1' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
@@ -36,6 +36,7 @@ define( 'DB_CHARSET', 'utf8mb4' );
 
 /** The database collate type. Don't change this if in doubt. */
 define( 'DB_COLLATE', '' );
+define('JWT_AUTH_SECRET_KEY', 'your-top-secret-key');
 
 /**#@+
  * Authentication unique keys and salts.
